f5ba5fba68e5dc10ab52fcf06dd9690098fdd56d
Atheer Alshahrani <utherr120@gmail.com>
2026-03-02 03:02:02 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from panda import Panda
+
+# Create 4 objects
+panda1 = Panda("Po", 5, "Black & White", 100)
+panda2 = Panda("Luna", 3, "Black & White", 80)
+panda3 = Panda("Milo", 7, "Black & White", 120)
+panda4 = Panda("Bella", 4, "Black & White", 90)
+
+# Print 1 attribute value
+print(panda1.name)
+
+# Call methods on each object
+panda1.eat()
+panda1.sleep()
+
+panda2.eat()
+panda2.sleep()
+
+panda3.eat()
+panda3.sleep()
+
+panda4.eat()
+panda4.sleep()
